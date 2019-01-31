3a94f70ce2150a7563845af183dace9d5f9b7672
fanrendale <1053314919@qq.com>
2019-01-31 10:03:18 +0800

    初始化项目

---
+++ src/main/java/com/xjf/springboot/SpringBootGradleElasticsearchApplication.java
@@ -0,0 +1,14 @@
+package com.xjf.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGradleElasticsearchApplication.class, args);
+    }
+
+}
+
